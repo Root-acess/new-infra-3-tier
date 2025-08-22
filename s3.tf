f96fded01f6037350e8c5efacd20b20c8e489c6f
swapbathu <selenium126@gmail.com>
2025-08-22 17:06:21 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,9 @@
-resource "aws_s3_bucket" "example_bucket" { bucket = "swiggy-bucket-terraform-fordemoinfra3tirerrrrfor4pmbatch" # Enable versioning versioning { enabled = true } }
+# Create an S3 Bucket
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "swiggy-bucket-terraform-fordemoinfra3tirerrrrfor4pmbatch"
+
+  # Enable versioning
+  versioning {
+    enabled = true
+  }
+}
